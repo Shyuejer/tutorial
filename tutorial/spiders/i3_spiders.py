1177fe86dff8a1e138a6a874c87a33e128d15969
Ng Shyue Jer <ngshyuejer@gmail.com>
2018-05-13 01:41:42 +0800

    Update i3_spiders.py

--- tutorial/spiders/i3_spiders.py
+++ tutorial/spiders/i3_spiders.py
@@ -26,7 +26,6 @@ class QuotesSpider(scrapy.Spider):
 'https://klse.i3investor.com/servlets/stk/5031.jsp',
 'https://klse.i3investor.com/servlets/stk/3336.jsp',
 'https://klse.i3investor.com/servlets/stk/1651.jsp',
-'https://klse.i3investor.com/servlets/stk/5099.jsp',
 'https://klse.i3investor.com/servlets/stk/0138.jsp',
 'https://klse.i3investor.com/servlets/stk/5099.jsp',
 'https://klse.i3investor.com/servlets/stk/1023.jsp',
